71cce5dd3b19b7da77c5394a8436cf70b70eb13e
Nithin G <79520840+2nithing@users.noreply.github.com>
2023-12-29 14:38:00 +0000

    final

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+@app.route('/name', methods=['GET','POST'])
+def hi():
+    if request.method=='POST':
+        num1 = request.form['num1']
+        result = factorial(int(num1))
+    return render_template('name.html',num2=result,num1=num1)
+
+def factorial(num):
+    i=1
+    fact=1
+    while i<=num:
+        fact = fact*i
+        i+=1
+    return fact
+
 
 
 if __name__=='__main__':
